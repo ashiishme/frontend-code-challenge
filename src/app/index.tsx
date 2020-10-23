5c09812744f0fda54776e2f3d36217fe6f682c12
ashiishme <logiicode@outlook.com>
2020-10-24 00:47:20 +0545

    chore: TODO!

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+const App: FC = () => {
+    return (
+        <div className='app-container'>
+            <p>Hello, there.</p>
+        </div>
+    );
+};
+
+export default App;
