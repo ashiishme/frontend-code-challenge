e50ad9ec05687caa104f9803c9bd39eb79dd9a2b
ashiishme <logiicode@outlook.com>
2020-10-25 00:34:30 +0545

    refactor: update app layout

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,12 @@
 import React, { FC } from 'react';
+import Sidebar from 'app/layout/sidebar';
+import Main from 'app/layout/main';
 
 const App: FC = () => {
     return (
         <div className='app-container'>
-            <p>Hello, there.</p>
+            <Sidebar />
+            <Main />
         </div>
     );
 };
