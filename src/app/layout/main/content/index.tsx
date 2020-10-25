4d5227bf01dfc9a3bbdddc9a0f718fab621b9294
ashiishme <logiicode@outlook.com>
2020-10-25 17:40:37 +0545

    refactor: conditionally render content area

--- src/app/layout/main/content/index.tsx
+++ src/app/layout/main/content/index.tsx
@@ -4,18 +4,27 @@ import styled from '@emotion/styled';
 import ContentHeader from 'app/components/content/header';
 import ContentBody from 'app/components/content/body';
 import PackageList from 'app/components/content/body/package';
+import { useContentHeaderContext } from 'app/hooks';
 
 const SectionContent = styled.div`
     margin-top: 40px;
 `;
 
 const Content: FC = () => {
+    const { context } = useContentHeaderContext();
+
     return (
         <SectionContent className='section-content section-packages container section-pad-r'>
             <div className='section-pacakges-wrapper section-pad-l'>
                 <ContentHeader />
                 <ContentBody>
-                    <PackageList />
+                    {context === 'packages' ? (
+                        <PackageList />
+                    ) : context === 'referral' ? (
+                        <h1>Referral</h1>
+                    ) : (
+                        <strong>No content found</strong>
+                    )}
                 </ContentBody>
             </div>
         </SectionContent>
