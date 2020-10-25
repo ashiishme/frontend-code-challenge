91340f5d95aad079e33018cdc315f613ec2da59e
ashiishme <logiicode@outlook.com>
2020-10-25 13:49:48 +0545

    refactor: package list type definition

---
+++ src/@types/components/components.d.ts
@@ -0,0 +1,15 @@
+/**
+ * Type Definition for all the components
+ *
+ * Include all the common types that are re-used as long as
+ * they don't requires specific definition file for large type definition
+ */
+
+/**
+ * Package item
+ */
+interface PackageItem {
+    title: string;
+    description: string;
+    price: string;
+}
